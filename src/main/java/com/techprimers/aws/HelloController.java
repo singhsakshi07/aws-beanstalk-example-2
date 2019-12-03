8f875a1dbab0e431cf6dfe6a19d5c848b89828b5
Sakshi Singh <34733494+singhsakshi07@users.noreply.github.com>
2019-12-03 18:38:46 +0530

    testing

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Welcome to ZombieLand";
+        return "Welcome to Greater Noida, The Green City";
     }
 }
