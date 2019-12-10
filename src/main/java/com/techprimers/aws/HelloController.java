fac580fba15b3c234893f2a097c44d7b5ac42e05
Sakshi Singh <34733494+singhsakshi07@users.noreply.github.com>
2019-12-10 16:27:21 +0530

    Testing AWS -CI CD

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Welcome to Greater Noida, The Green City";
+        return "Welcome to the new AWS-Cloud world";
     }
 }
